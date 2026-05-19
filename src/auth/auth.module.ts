269533f3a33ddcb4957edfa2eac998a232c3293d
gregorydev97 <gregoryramirez97@hotmail.com>
2026-05-18 21:20:14 -0400

    Added jwt functionality

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import type { StringValue } from 'ms';
 import { UsersModule } from 'src/users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+  throw new Error('JWT_SECRET is not defined');
+}
+
+const jwtExpiresIn = (process.env.JWT_EXPIRES_IN || '1d') as StringValue;
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: jwtExpiresIn,
+      },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
 })
